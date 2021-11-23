a86eb523d313421611a52d49cc7d9dc276843ad6
Webpageproofs <94922323+Webpageproofs@users.noreply.github.com>
2021-11-24 01:53:28 +0700

    Flaby

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//@prepros-append script.js
+//@prepros-append jq.js
+// <ИЗОБРАЖЕНИЕ В HTML И В BACKGROUND URL>
+function ibg(){
+
+var ibg = document.querySelectorAll(".ibg");
+for (var i = 0; i < ibg.length; i++) {
+if(ibg[i].querySelector('img')){
+ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+}
+}
+}
+
+ibg();
+
+// </ИЗОБРАЖЕНИЕ В HTML И В BACKGROUND URL>
+
+
+
+$(document).ready(function (){
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		items: 1,
+		autoplay:true,
+		navText: ["<img src=\"img/icons/14.png\" alt=\"img\" >","<img src=\"img/icons/15.png\" alt=\"img\" >"],
+	});
+});
